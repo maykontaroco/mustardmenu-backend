eb2327243b23e38a0c94738d8eba8c93fd27df3f
Maykon Taroco <maykontaroco@gmail.com>
2023-07-06 22:19:15 -0300

    Initial commit

---
+++ src/main/java/br/com/taroco/mustardmenu/MustardMenuApplication.java
@@ -0,0 +1,13 @@
+package br.com.taroco.mustardmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustardMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MustardMenuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/taroco/mustardmenu/MustardMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.taroco.mustardmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MustardMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
